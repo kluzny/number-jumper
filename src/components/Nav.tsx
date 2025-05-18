60292c740a5900c36cd10a979f165330e678283c
Kyle Luzny <kluzny@gmail.com>
2025-05-18 18:25:36 -0500

    gut the boilerplate and add tailwind

---
+++ src/components/Nav.tsx
@@ -0,0 +1,11 @@
+function Nav() {
+    return (
+        <nav className="bg-green-500 w-full">
+            <h1 className="inline-block text-2xl text-white font-bold p-4">
+                Number Jumper!
+            </h1>
+        </nav>
+    )
+}
+
+export default Nav
