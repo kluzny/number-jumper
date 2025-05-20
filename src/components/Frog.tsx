a419b1e5a7ff52f2fddce75a38527492e31ca44d
Kyle Luzny <kluzny@gmail.com>
2025-05-19 19:15:20 -0500

    WIP: react-responsive-spritesheet, but its a bit janky

--- src/components/Frog.tsx
+++ src/components/Frog.tsx
@@ -1,7 +1,21 @@
+import Spritesheet from "react-responsive-spritesheet";
+import frogIdle from "../assets/sprites/frog_idle.png";
+
 function Frog() {
   return (
     <>
-      <div className="frog h-20 w-20 m-2 bg-green-400"></div>
+      <div className="frog h-20 w-20 m-2 bg-green-400">
+        <Spritesheet
+          image={frogIdle}
+          widthFrame={32}
+          heightFrame={32}
+          steps={2}
+          fps={4}
+          direction={"forward"}
+          loop={true}
+          autoplay={true}
+        />
+      </div>
     </>
   );
 }
